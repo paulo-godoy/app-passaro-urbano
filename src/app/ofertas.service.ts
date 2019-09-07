3840ee61095f18d0fd9317168451820b0ba9f721
Paulo Godoy <pauloricardo.godoy@yahoo.com.br>
2019-09-07 00:16:56 -0300

    recuperando ofertas restaurante httprequest api rest

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -7,14 +7,20 @@ import { Oferta } from './shared/oferta.model';
 export class OfertasService {
 
     constructor(private http: Http) {}
-    
 
-    
+
+
     public getOfertas(): Promise<Oferta[]> {
         //Efetuar uma requisição http
         return this.http.get('http://localhost:3000/ofertas?destaque=true')
             .toPromise()
             .then((resposta: any) => resposta.json())
         //retornar uma promise Ofertas[]
+    }
+
+    public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]> {
+      return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        .toPromise()
+        .then((resposta: any) => resposta.json())
     }
 }
