3b39054bed16ea3c47dc0f0aa09a0bd51f1ff1d8
Paulo Godoy <pauloricardo.godoy@yahoo.com.br>
2019-09-10 15:48:35 -0300

    Ajustando template do ofertaComponent

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,17 +1,31 @@
+import { Oferta } from './../shared/oferta.model';
+import { OfertasService } from './../ofertas.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute} from '@angular/router'
+import { ActivatedRoute} from '@angular/router';
+
+
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [ OfertasService ]
 })
 export class OfertaComponent implements OnInit {
 
-    constructor(private route: ActivatedRoute) { }
+    public oferta: Oferta
+
+    constructor(
+      private route: ActivatedRoute,
+      private ofertaService: OfertasService
+      ) { }
 
   ngOnInit() {
-    console.log(this.route.snapshot.params['id'])
+   this.ofertaService.getOfertaPorId(this.route.snapshot.params['id'])
+    .then(( oferta: Oferta ) => {
+      this.oferta = oferta
+    })
+
   }
 
 }
