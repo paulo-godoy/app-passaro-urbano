254617a3d52375834cb289b049de2e436cac426e
Paulo Godoy <pauloricardo.godoy@yahoo.com.br>
2019-09-10 16:19:28 -0300

    ajustando a url de acesso a api

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,6 +1,7 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Oferta } from './shared/oferta.model';
+import { URL_API } from './app.api';
 // import 'rxjs/add/operator/toPromise';
 
 @Injectable()
@@ -12,20 +13,20 @@ export class OfertasService {
 
     public getOfertas(): Promise<Oferta[]> {
         //Efetuar uma requisição http
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${URL_API}?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta.json())
         //retornar uma promise Ofertas[]
     }
 
     public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]> {
-      return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+      return this.http.get(`${URL_API}?categoria=${categoria}`)
         .toPromise()
         .then((resposta: any) => resposta.json())
     }
 
     public getOfertaPorId(id: number): Promise<Oferta> {
-      return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+      return this.http.get(`${URL_API}?id=${id}`)
       .toPromise()
       .then((resposta: any) => {
         return resposta.json()[0]
